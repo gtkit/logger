4afd7dbe8f711d8a34ee6d18041f5aaca8b5b576
xiaozhaofu <xiaozhaofu@163.com>
2023-01-30 17:49:54 +0800

    fix:添加日志方法

--- logger.go
+++ logger.go
@@ -195,19 +195,33 @@ func ZInfo(msg string, fields ...zap.Field) {
 	zlog.Info(msg, fields...)
 }
 
+// LogInfoIf 当 err != nil 时记录 info 等级的日志
+func LogInfoIf(err error) {
+	if err != nil {
+		zlog.Info("Error Occurred:", zap.Error(err))
+	}
+}
+
 func Infof(format string, args ...interface{}) {
 	zlog.Sugar().Infof(format, args...)
 }
 func Warn(args ...interface{}) {
 	zlog.Sugar().Warn(args...)
 }
-
+func ZWarn(moduleName string, fields ...zap.Field) {
+	zlog.Warn(moduleName, fields...)
+}
 func Warnf(format string, args ...interface{}) {
 	zlog.Sugar().Warnf(format, args...)
 }
 
 func Error(args ...interface{}) {
 	zlog.Sugar().Error(args...)
+}
+
+// Error
+func ZError(moduleName string, fields ...zap.Field) {
+	zlog.Error(moduleName, fields...)
 }
 
 func Errorf(format string, args ...interface{}) {
@@ -234,10 +248,22 @@ func Fatal(args ...interface{}) {
 	zlog.Sugar().Fatal(args...)
 }
 
+// Fatal 级别同 Error(), 写完 log 后调用 os.Exit(1) 退出程序
+func ZFatal(moduleName string, fields ...zap.Field) {
+	zlog.Fatal(moduleName, fields...)
+}
+
 func Fatalf(format string, args ...interface{}) {
 	zlog.Sugar().Fatalf(format, args...)
 }
 
 func Infow(msg string, keysAndValues ...interface{}) {
 	zlog.Sugar().Infow(msg, keysAndValues...)
+}
+
+// LogIf 当 err != nil 时记录 error 等级的日志
+func LogIf(err error) {
+	if err != nil {
+		zlog.Error("Error Occurred:", zap.Error(err))
+	}
 }
