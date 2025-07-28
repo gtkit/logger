3505deb465ce2e68e3752651eb5f958cf70993ad
xiaozhaofu <xiaozhaofu@163.com>
2025-07-28 16:27:18 +0800

    添加发送消息方法

--- news.go
+++ news.go
@@ -1,8 +1,30 @@
 package logger
 
+import (
+	"fmt"
+)
+
 var newser Newser
 
 type Newser interface {
 	Text(args ...any)
 	TextF(template string, args ...any)
+}
+
+// Message format with Sprint, Sprintf, or neither.
+func Message(template string, fmtArgs []any) string {
+	if len(fmtArgs) == 0 {
+		return template
+	}
+
+	if template != "" {
+		return fmt.Sprintf(template, fmtArgs...)
+	}
+
+	if len(fmtArgs) == 1 {
+		if str, ok := fmtArgs[0].(string); ok {
+			return str
+		}
+	}
+	return fmt.Sprint(fmtArgs...)
 }
